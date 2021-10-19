5b09a414360ec4108a28f9f609b9103ded11c8c3
sivertheisholt <sivert.b.heisholt@protonmail.com>
2021-10-19 23:10:05 +0200

    MEssage is now handled on backend

--- controllers/collabController.js
+++ controllers/collabController.js
@@ -13,7 +13,16 @@ exports.collab_get = async function(req, res) {
 
 exports.collab_set = async function(req, res) {
     try {
-        collabHandler.collab_set(req.query.setter, req.query.collab);
+        let people = req.query.collab.split(':');
+        let stringPeople = 'ic3husky is currently playing with '
+        let stringTwitch = 'you can find their content over at '
+        for(let i = 0; i < people.length; i++){
+            stringPeople += `${people[i]} `;
+            if(people[i].indexOf('@')> -1) {
+                stringTwitch += `${i == 0 ? '' : ' | '}https://www.twitch.tv/${people[i].replace('@', '')}`
+            }
+        }
+        collabHandler.collab_set(req.query.setter, stringPeople + stringTwitch);
         res.status(200).send(`Thank you for updating the collab ${req.query.setter}!`);
     } catch(err) {
         res.status(400).send('Something went wrong setting collab');
